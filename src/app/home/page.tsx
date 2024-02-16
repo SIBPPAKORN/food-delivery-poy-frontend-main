15ef77b333c4eef0fe345787c69885c8b10f0e27
poy <ghhh7731@gmail.com>
2024-02-16 17:51:29 +0700

    feat: :memo: new layout

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { NextPage } from "next";
+
+export const Home: NextPage = () => {
+	return <main className="h-screen w-full flex overflow-auto">page</main>;
+};
+
+export default Home;
